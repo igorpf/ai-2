151aed254c32e36516cb5657e55df555d8fe115c
marcosps96 <marcos_souza96@hotmail.com>
2016-10-28 18:42:58 -0200

    Corrige valor de ep_ended

--- Simulator/simulator.py
+++ Simulator/simulator.py
@@ -554,6 +554,7 @@ def game(mode, load, bot_type):
 					episode += 1
 					
 		if mode == "learn":
+			ep_ended = True
 			reward = controller.compute_reward(action, prev_state, controller.state, actions, ep_ended, final_event)
 			total_rw += reward
 			controller.updateQ(action, prev_state, controller.state, reward, ep_ended)
